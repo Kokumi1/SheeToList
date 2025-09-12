daac257e7e9f5d7cc38bc96decdbc3bfb58d6cd7
Naplucia <72755048+Kokumi1@users.noreply.github.com>
2025-09-12 20:44:42 +0200

    Finitions de l'interface avec: bouton lié au Viewmodel avec réaction ajout du bouton Add (+)

--- SheeToList/MainPage.xaml.cs
+++ SheeToList/MainPage.xaml.cs
@@ -1,30 +1,46 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using SheeToList.Model;
 using SheeToList.Services;
 
 namespace SheeToList
 {
     public partial class MainPage : ContentPage
     {
-        
-        string dataString = "";
+        //MainViewModel viewModel;
 
         public MainPage()
         {
             InitializeComponent();
+            //viewModel = new MainViewModel();
             BindingContext = new MainViewModel();
-            var Items = new MainViewModel().Items;
-         
+            //var Items = new MainViewModel().Items;
+
         }
     }
 
     public class MainViewModel : INotifyPropertyChanged
     {
+        private bool _isFilterVisible;
+        public bool IsFilterVisible
+        {
+            get => _isFilterVisible;
+            set
+            {
+                _isFilterVisible = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FilterShowButtonText));
+            }
+        }
+
         public ObservableCollection<Item> Items { get; }
 
         public MainViewModel()
         {
+            FilterShowCommand = new Command(() => IsFilterVisible = !IsFilterVisible);
+            AddItemCommand = new Command(() => AddItem("added thing"));
             //initialize Google Sheets API
             /*    GoogleApiTalker apiTalker = new();
                dataString =   apiTalker.GetDataString();
@@ -39,29 +55,18 @@ namespace SheeToList
             OnPropertyChanged();
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        void OnPropertyChanged([CallerMemberName] string name = null) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-    }
-    public class Item : INotifyPropertyChanged
-    {
-        string text;
-        bool isChecked;
-
-        public string Text
+        public void AddItem(string text)
         {
-            get => text;
-            set { text = value; OnPropertyChanged(); }
+            Items.Add(new Item { Text = text, IsChecked = false });
+            OnPropertyChanged(nameof(Items));
         }
 
-        public bool IsChecked
-        {
-            get => isChecked;
-            set { isChecked = value; OnPropertyChanged(); }
-        }
+        public ICommand AddItemCommand { get; }
+        public ICommand FilterShowCommand { get; }
+        public string FilterShowButtonText => IsFilterVisible ? "Cachez" : "Révélez tout";
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        void OnPropertyChanged([CallerMemberName] string name = null) =>
+        public event PropertyChangedEventHandler? PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string name = "") =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
---
+++ SheeToList/Model/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SheeToList.Model
+{
+    //TODO: rename: class name to Product
+    public class Item : INotifyPropertyChanged
+    {
+        string text;
+    bool isChecked;
+
+    public string Text
+    {
+        get => text;
+        set { text = value; OnPropertyChanged(); }
+    }
+
+    public bool IsChecked
+    {
+        get => isChecked;
+        set { isChecked = value; OnPropertyChanged(); }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    void OnPropertyChanged([CallerMemberName] string name = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
+}
