d606f965131ab4569fd5808caf4b4353722a8c58
Naplucia <72755048+Kokumi1@users.noreply.github.com>
2025-09-22 17:57:37 +0200

    Refactorisation du ViewModel dans MainPage.xaml.cs

--- SheeToList/MainPage.xaml.cs
+++ SheeToList/MainPage.xaml.cs
@@ -9,6 +9,7 @@ namespace SheeToList
 {
     public partial class MainPage : ContentPage
     {
+        #region MainPage
         public MainPage()
         {
             InitializeComponent();
@@ -21,107 +22,121 @@ namespace SheeToList
             return  await DisplayPromptAsync(title, message, initialValue: initialValue);
         }
     }
+    #endregion
 
+
+    // ViewModel for MainPage
     public class MainViewModel : INotifyPropertyChanged
     {
         private readonly MainPage _page ;
-        private bool _isFilterVisible ;
-        public bool IsFilterVisible
+        private bool _isbuyedProductVisible ;
+
+        #region Properties
+        public bool IsbuyedProductVisible
         {
-            get => _isFilterVisible;
+            get => _isbuyedProductVisible;
             set
             {
-                _isFilterVisible = value;
+                _isbuyedProductVisible = value;
                 OnPropertyChanged();
                 OnPropertyChanged(nameof(FilterShowButtonText));
-                OnPropertyChanged(nameof(FilteredItems));
+                OnPropertyChanged(nameof(ToBuyProducts));
             }
         }
 
-        public ObservableCollection<ProductToBuy> FilteredItems
+        public ObservableCollection<ProductToBuy> ToBuyProducts
         {
             get
             {
-                if (IsFilterVisible)
-                    return Items;
-                return new ObservableCollection<ProductToBuy>(Items.Where(item => !item.IsChecked));
+                if (IsbuyedProductVisible)
+                    return Products;
+                return new ObservableCollection<ProductToBuy>(Products.Where(item => !item.IsChecked));
             }
         }
+        public ObservableCollection<ProductToBuy> Products { get; set; }
+        #endregion
 
-        public ObservableCollection<ProductToBuy> Items { get; set; }
-
+        #region Constructor
         public MainViewModel(MainPage page)
         {
-            FilterShowCommand = new Command(() => IsFilterVisible = !IsFilterVisible);
-            AddItemCommand = new Command(AddItem);
+            FilterShowCommand = new Command(() => IsbuyedProductVisible = !IsbuyedProductVisible);
+            AddItemCommand = new Command(AddProduct);
             ImportDataCommand = new Command(() => ImportData());
-            EditItemCommand = new Command<ProductToBuy>(EditItem);
-            DeleteItemCommand = new Command<ProductToBuy>(DeleteItem);
+            EditItemCommand = new Command<ProductToBuy>(EditProduct);
+            DeleteItemCommand = new Command<ProductToBuy>(DeleteProduct);
 
-            Items = [];
+            Products = [];
             _page = page;
-
-            Items.CollectionChanged += (s, e) =>
-            {
-                if(e.NewItems != null)
-                    foreach (ProductToBuy item in e.NewItems)
-                        item.PropertyChanged += Item_PropertyChanged;
-                    if(e.OldItems != null)
-                        foreach (ProductToBuy item in e.OldItems)
-                            item.PropertyChanged -= Item_PropertyChanged;
-                OnPropertyChanged(nameof(FilteredItems));
-            };
+            CollectionChangedSetup();
         }
+        #endregion
 
-        public async void AddItem()
+        #region Data Import
+        public void ImportData()
         {
-            string? text = await _page.ItemNameAskerAsync("Entrer le nom", "Entrer le nom de l'objet à ajoutée");
-            if (string.IsNullOrWhiteSpace(text)) return;
+            GoogleApiTalker apiTalker = new();
 
-            Items.Add(new ProductToBuy { Name = text, IsChecked = false });
-            OnPropertyChanged(nameof(Items));
+            var sorted = GoogleApiTalker.GetData().OrderBy(item => item.Name).ToList();
+            foreach (var item in sorted)
+                Products.Add(item);
+            OnPropertyChanged(nameof(Products));
         }
+        #endregion
 
+        #region Command Region
         public ICommand AddItemCommand { get; }
         public ICommand FilterShowCommand { get; }
         public ICommand ImportDataCommand { get; }
         public ICommand EditItemCommand { get; }
         public ICommand DeleteItemCommand { get; }
-        public string FilterShowButtonText => IsFilterVisible ? "Cachez" : "Révélez tout";
+        public string FilterShowButtonText => IsbuyedProductVisible ? "Cachez" : "Révélez tout";
+        #endregion
 
-        public void ImportData()
+        #region Products Management
+        public async void AddProduct()
         {
-            GoogleApiTalker apiTalker = new();
-           
-            var sorted = GoogleApiTalker.GetData().OrderBy(item => item.Name).ToList();
-            foreach (var item in sorted)
-                Items.Add(item);
-
-           
-            OnPropertyChanged(nameof(Items));
-        }
-
-        private async void EditItem(ProductToBuy item)
-        {
-            if (item == null) return;
-            // Logique d'édition (ex: ouvrir une popup de modification)
-            string? text = await _page.ItemNameAskerAsync("Entrer le nom", "Entrer le nouveau nom",
-                item.Name);
+            string? text = await _page.ItemNameAskerAsync("Entrer le nom", "Entrer le nom de l'objet à ajoutée");
             if (string.IsNullOrWhiteSpace(text)) return;
-            item.Name = text;
-        }
 
-        private void DeleteItem(ProductToBuy item)
+            Products.Add(new ProductToBuy { Name = text, IsChecked = false });
+            OnPropertyChanged(nameof(Products));
+        }
+        private async void EditProduct(ProductToBuy Product)
         {
-            if (item == null) return;
-            Items.Remove(item);
+            if (Products == null) return;
+            string? text = await _page.ItemNameAskerAsync("Entrer le nom", "Entrer le nouveau nom",
+                Product.Name);
+            if (string.IsNullOrWhiteSpace(text)) return;
+            Product.Name = text;
         }
 
+        private void DeleteProduct(ProductToBuy Product)
+        {
+            if (Products == null) return;
+            Products.Remove(Product);
+        }
+        #endregion
+
+        private void CollectionChangedSetup()
+        {
+            Products.CollectionChanged += (s, e) =>
+            {
+                if (e.NewItems != null)
+                    foreach (ProductToBuy item in e.NewItems)
+                        item.PropertyChanged += Item_PropertyChanged;
+                if (e.OldItems != null)
+                    foreach (ProductToBuy item in e.OldItems)
+                        item.PropertyChanged -= Item_PropertyChanged;
+                OnPropertyChanged(nameof(ToBuyProducts));
+            };
+        }
+
+        #region Event Handlers
         private void Item_PropertyChanged(object? sender, PropertyChangedEventArgs e)
         {
             if (e.PropertyName == nameof(ProductToBuy.IsChecked))
             {
-                OnPropertyChanged(nameof(FilteredItems));
+                OnPropertyChanged(nameof(ToBuyProducts));
             }
         }
 
@@ -129,4 +144,5 @@ namespace SheeToList
         void OnPropertyChanged([CallerMemberName] string name = "") =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
+    #endregion
 }
